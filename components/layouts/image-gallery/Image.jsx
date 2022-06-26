f5bb51d90f56895ecdece0dee1eeaa84b1ab1166
videsh <videsh.ghodarop@iaugmentor.com>
2022-06-26 13:09:12 +0530

    added some modification

--- components/layouts/image-gallery/Image.jsx
+++ components/layouts/image-gallery/Image.jsx
@@ -1,39 +1,39 @@
-import React, { useState, useEffect } from "react";
-import Header from "../../Header";
-import Loader from "../../Loader";
-import Unsplash from "../../Unsplash";
-import axios from "axios";
-import InfiniteScroll from "react-infinite-scroll-component";
+import React, { useState, useEffect } from 'react';
+import Header from '../../Header';
+import Loader from '../../Loader';
+import Unsplash from '../../Unsplash';
+import axios from 'axios';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
-const Image = () => {
-  const [images, setImages] = useState([]);
+const Image = ({ imagesData }) => {
+  const [images, setImages] = useState(imagesData);
 
-  useEffect(() => {
-    fetchImages();
-  }, []);
+  // useEffect(() => {
+  //   fetchImages();
+  // }, []);
 
-  const fetchImages = async () => {
-    const res = await axios.get(
-      "https://api.unsplash.com/photos/random?client_id=9cUKmF10zR5nvb8EEdkLjD2j284E1fMXB8EHCXuUNOE&count=10"
-    );
-    setImages([...images, ...res.data]);
-  };
+  // const fetchImages = async () => {
+  //   const res = await axios.get(
+  //     "https://api.unsplash.com/photos/random?client_id=9cUKmF10zR5nvb8EEdkLjD2j284E1fMXB8EHCXuUNOE&count=10"
+  //   );
+  //   setImages([...images, ...res.data]);
+  // };
 
   return (
     <div>
       <Header />
-      <InfiniteScroll
+      {/* <InfiniteScroll
         dataLength={images.length}
         next={fetchImages}
         hasMore={true}
         loader={<Loader />}
-      >
-        <div className="w-[80%] mx-auto m-[4rem] grid gap-[1em] grid-cols-4 auto-rows-[300px]">
-          {images.map((image) => (
-            <Unsplash url={image.urls.thumb} key={image.id} />
-          ))}
-        </div>
-      </InfiniteScroll>
+      > */}
+      <div className="w-[80%] mx-auto m-[4rem] grid gap-[1em] grid-cols-4 auto-rows-[300px]">
+        {images.map((image) => (
+          <Unsplash url={image.url} key={image.id} />
+        ))}
+      </div>
+      {/* </InfiniteScroll> */}
     </div>
   );
 };
---
+++ pages/image-gallery.jsx
@@ -0,0 +1,43 @@
+import { NextPage } from 'next';
+import Image from '../components/layouts/image-gallery/Image';
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+} from 'firebase/firestore';
+import { projectFirestore } from '../firebase/config';
+
+const ImageGallery = ({ imagesData }) => {
+  console.log(imagesData);
+  return (
+    <>
+      <div className="bg-[#fffbeb]">
+        <Image imagesData={imagesData} />
+      </div>
+    </>
+  );
+};
+
+export default ImageGallery;
+
+export async function getStaticProps(context) {
+  const imagesRef = collection(projectFirestore, 'images');
+  const q = await query(imagesRef, orderBy('timestamp', 'desc'), limit(8));
+  const res = await getDocs(q);
+  // const imagesRef = collection(projectFirestore, 'images');
+  // const res = await getDocs(imagesRef);
+  let documents = [];
+  res.forEach((doc) => {
+    documents.push({ ...doc.data(), id: doc.id });
+  });
+
+  return {
+    props: {
+      imagesData: JSON.parse(JSON.stringify(documents)),
+    }, // will be passed to the page component as props
+  };
+}
